33ac6d43cdee677144f733038641c98193dbcd9e
JB <jb>
2023-01-16 18:35:22 +0100

    Builder MVP architecture on a simple example.

--- app/src/main/java/com/android/gk_popular_libraries/MainActivity.kt
+++ app/src/main/java/com/android/gk_popular_libraries/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.android.gk_popular_libraries
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.gk_popular_libraries.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), MainView {
+
+    private lateinit var binding: ActivityMainBinding
+    private val presenter = CountersPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            btnCounter1.setOnClickListener { presenter.onCLickFirstButton(0) }
+            btnCounter2.setOnClickListener { presenter.onClickSecondButton(1) }
+            btnCounter3.setOnClickListener { presenter.onClickThirdButton(2) }
+        }
+    }
+
+    override fun setTextFirstBtn(text: String) {
+        binding.btnCounter1.text = text
+    }
+
+    override fun setTextSecondBtn(text: String) {
+        binding.btnCounter2.text = text
+    }
+
+    override fun setTextThirdBtn(text: String) {
+        binding.btnCounter3.text = text
     }
 }
---
+++ app/src/main/java/com/android/gk_popular_libraries/MainView.kt
@@ -0,0 +1,7 @@
+package com.android.gk_popular_libraries
+
+interface MainView {
+    fun setTextFirstBtn(text: String)
+    fun setTextSecondBtn(text: String)
+    fun setTextThirdBtn(text: String)
+}
